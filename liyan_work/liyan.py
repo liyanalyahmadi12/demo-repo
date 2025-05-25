bb2fcf3afce6bccfe0b8f709a96da79bc5749f3e
liyanalyahmadi12 <layanalyahmadie@gmail.com>
2025-05-25 19:54:48 +0400

    testing waters

--- liyan_work/liyan.py
+++ liyan_work/liyan.py
@@ -1,1 +1,19 @@
-print("hello world!!!!!!!!!!")
+
+list_of_num = []
+
+while True:
+    i_said = input("Write down numbers and enter 'exist' to view even numbers: ")
+    
+    if i_said == 'exist':
+        break
+    
+    number = int(i_said)
+    list_of_num.append(number)
+    print("Please enter a valid number or 'exist' to quit.")
+
+for num in list_of_num:
+    if num % 2 == 0:
+        print("Even number:", num)
+
+
+
