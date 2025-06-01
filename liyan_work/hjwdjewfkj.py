efa9d2f447cd12cc840f113dc8b9bcc3262a0124
liyanalyahmadi12 <layanalyahmadie@gmail.com>
2025-06-01 20:34:28 +0400

    our practice

---
+++ liyan_work/hjwdjewfkj.py
@@ -0,0 +1,17 @@
+f =202
+print(id(f))
+f=403
+print(id(f))
+
+
+length= 15
+print(length)
+prices = [12,4,56,89]
+print(prices)
+name, price, quantity = 'soap', 12.5,7
+print(name, price, quantity)
+a,b,c = 1,1,1,
+print(id(a),id(b), id(c))
+r = 5
+r=4.14
+print(r)
